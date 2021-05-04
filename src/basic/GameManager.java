b095aa4b8b890e095f688012241467a5a13d474a
LH <hohmann.lorenz99@gmail.com>
2021-05-04 14:32:07 +0200

    Basic GUI implemented

--- src/basic/GameManager.java
+++ src/basic/GameManager.java
@@ -9,10 +9,11 @@ public class GameManager {
 
 	private static Player player;
 	private Map mainMap;
-	
+	private static GameManager instance;
 	private GUIManager guiManager;
 	
 	public GameManager() {
+		instance = this;
 		this.prepareGame();
 	}
 	
@@ -26,5 +27,14 @@ public class GameManager {
 	
 	public static Player getPlayer() {
 		return player;
+	}
+	
+	public void restart() {
+		this.guiManager.destroy();
+		this.prepareGame();
+	}
+	
+	public static GameManager getInstance() {
+		return instance;
 	}
 }
