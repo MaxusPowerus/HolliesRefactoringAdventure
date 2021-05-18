f547a9a4e4b8a159b847f83a4ca340493c951ade
LH <hohmann.lorenz99@gmail.com>
2021-05-18 13:56:19 +0200

    fixed merge conflicts

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -45,32 +45,22 @@ public class Player {
 		this.weapon = null;
 
 		// Set StartInventory
-<<<<<<< HEAD
 		Weapon startWeapon = new Weapon("Stick", "Stock", 1, 1);
-		Outfit startOutfit = new Outfit("HolliesDress", "Hollys Lieblingskleid", 0, 10);
-		Food startFood = new Food("StewGrandmaStyle", "Eintopf nach Omas Art", 25, 1);
-		Note startNote = new Note("LetterFromHolger01", "Nachricht von Holger",
-				"Hey Holly!\nTriff mich heute Abend im Wald!\nIch habe eine Überraschung für dich!\nDein Holger <3", 0);
-		Other startOther = new Other("HolliesTeddy", "SchrimplyPipples",
-				"Hollies ältester und wichtigster Begleiter: Ein alter abgenutzter Teddybär!", 0);
-=======
-		Weapon startWeapon = new Weapon("Stick", "Stock", 1, 0);
 		Outfit startOutfit = new Outfit("HolliesDress", "Hollys Lieblingskleid", 0, 10);
 		Food startFood = new Food("StewGrandmaStyle", "Eintopf nach Omas Art", 25, 1);
 		Note startNote = new Note("LetterFromHolger01", "Nachricht von Holger",
 				"Hey Holly!<br>Triff mich heute Abend im Wald!<br>Ich habe eine Überraschung für dich!<br>Dein Holger <3",
 				0);
->>>>>>> a2336b140edc7b0a4b9a7c17ead4ce5825b83358
+		Other startOther = new Other("HolliesTeddy", "SchrimplyPipples",
+				"Hollies ältester und wichtigster Begleiter: Ein alter abgenutzter Teddybär!", 0);
 
 		inventory.add(startWeapon);
 		inventory.add(startOutfit);
 		inventory.add(startFood);
 		inventory.add(startNote);
 		inventory.add(startOther);
-		;
 
 		inventory.addGold(5);
-
 		// ==============================================================================================
 	}
 
