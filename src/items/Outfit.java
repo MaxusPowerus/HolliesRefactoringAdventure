53aac839461756f99dfdb4aa466377ffa23f0a42
LH <hohmann.lorenz99@gmail.com>
2021-05-20 14:18:37 +0200

    added outfiteffects in gui

--- src/items/Outfit.java
+++ src/items/Outfit.java
@@ -1,23 +1,28 @@
 package items;
 
 import utilities.OutfitEffect;
+import utilities.Skill;
 
 public class Outfit extends Item {
 
 	private int armor;
 	private OutfitEffect outfitFx;
 
-	public Outfit(String uniqueName, String name, int value, int armor, int spwanChance, int stFx, int peFx, int enFx,
+	public Outfit(String uniqueName, String name, int value, int armor, int spawnChance, int stFx, int peFx, int enFx,
 			int chFx, int inFx, int alFx, int lkFx) {
-		super(uniqueName, name, value, spwanChance);
+		super(uniqueName, name, value, spawnChance);
 		this.armor = armor;
 		this.outfitFx = new OutfitEffect(stFx, peFx, enFx, chFx, inFx, alFx, lkFx);
 	}
 
-	public Outfit(String uniqueName, String name, int value, int armor, int spwanChance, OutfitEffect outfitFx) {
-		super(uniqueName, name, value, spwanChance);
-		this.armor = armor;
-		this.outfitFx = outfitFx;
+	public Outfit(String uniqueName, String name, int value, int armor, int spawnChance, OutfitEffect outfitFx) {
+		this(uniqueName, name, value, armor, spawnChance, outfitFx.getSkillBoost().getSkillValue(Skill.STRENGTH),
+				outfitFx.getSkillBoost().getSkillValue(Skill.PERCEPTION),
+				outfitFx.getSkillBoost().getSkillValue(Skill.ENDURANCE),
+				outfitFx.getSkillBoost().getSkillValue(Skill.CHARISMA),
+				outfitFx.getSkillBoost().getSkillValue(Skill.INTELLIGENCE),
+				outfitFx.getSkillBoost().getSkillValue(Skill.AGILITY),
+				outfitFx.getSkillBoost().getSkillValue(Skill.LUCK));
 	}
 
 	public int getArmor() {
