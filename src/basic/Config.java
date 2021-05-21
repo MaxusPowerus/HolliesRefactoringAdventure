8d34bfbf1f25870282a572926e6e139b3b195eda
Mux 'The one an only ultimate SCRUM-Master' Wunzel <@>
2021-05-21 09:29:20 +0200

    new JsonStructur added

--- src/basic/Config.java
+++ src/basic/Config.java
@@ -17,7 +17,7 @@ public class Config {
 	// startItems
 
 	// MAP
-	public final static int MAP_SIZEX = 50;
-	public final static int MAP_SIZEY = 50;
+	public final static int MAP_SIZEX = 100;
+	public final static int MAP_SIZEY = 100;
 
 }
